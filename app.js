8ce753e9daf2766348b62a36069b176b04026b63
Vuk Vidacic <vidacicvuk13@gmail.com>
2022-07-01 14:17:32 +0200

    Add CSS

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const bp = require("body-parser");
+const { application } = require("express");
 
 const app = express();
+var items = ["Eat","Cook"];
 
 app.set("view engine","ejs");
+app.use(bp.urlencoded({extended:true}))
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.send("Server is running!");
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day=today.toLocaleDateString("en-US",options)
+    res.render("list",{day: day,listItems:items})
+})
+
+app.post("/",(req,res)=>{
+    items.push(req.body.listItem)
+    res.redirect("/");
 })
 
 app.listen(3000,()=>{
